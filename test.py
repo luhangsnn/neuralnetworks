2330c1616795207f9e035b7add50f0ceea015588
luhangsnn <57969620+luhangsnn@users.noreply.github.com>
2020-09-07 15:35:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,20 @@ print("testing sync - client 2")
 print("testing sync issues - client 1")
 
 print("changes on master branch remote")
+
+def compute_accuracy(self, y, y_pred):
+        ''' Computes accuracy (proportion correct) (across a single training epoch)
+
+        Parameters:
+        ----------
+        y: ndarray. Shape = [Num samples N,]
+            True classes corresponding to each input sample in a training epoch  (coded as -1 or +1).
+        y_pred: ndarray. Shape = [Num samples N,]
+            Predicted classes corresponding to each input sample (coded as -1 or +1).
+
+        Returns:
+        ----------
+        float. The accuracy for each input sample in the epoch. ndarray.
+            Expressed as proportions in [0.0, 1.0]
+        '''
+        return (1 - np.mean(y_pred != y))
