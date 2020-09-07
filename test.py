ab24e1d9770f2b2d7973dd1ab592568d3b55327f
luhangsnn <57969620+luhangsnn@users.noreply.github.com>
2020-09-07 15:34:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,5 @@ print("test pulling and sync v2")
 
 print("testing sync - client 2")
 print("testing sync issues - client 1")
-<<<<<<< HEAD
 
-print("test local client")
-=======
->>>>>>> df55a84da3d00cf687b140f2d0d2d31bf27620cd
-
-print("test remote client")
->>>>>>> cd81b7851eab362c7e78413a1cd50abf53239db0
+print("changes on master branch remote")
