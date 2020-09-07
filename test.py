374635a161ce7666927c8609e5e9f54daad6f046
Luhang Sun <luhangsun@outlook.com>
2020-09-07 15:26:54 +0800

    testing change sync

--- test.py
+++ test.py
@@ -6,8 +6,7 @@ print("test v3")
 
 print("test pulling and sync v2")
 
-<<<<<<< HEAD
 print("testing sync - client 2")
-=======
 print("testing sync issues - client 1")
->>>>>>> df55a84da3d00cf687b140f2d0d2d31bf27620cd
+
+print("test local client")
