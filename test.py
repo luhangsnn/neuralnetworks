c792bcec8ddacdbd0c7d80f6be43dfd4efb59e05
Luhang Sun <luhangsun@outlook.com>
2020-09-07 15:34:30 +0800

    testing branch

--- test.py
+++ test.py
@@ -4,15 +4,9 @@ print("test new commit")
 
 print("test v3")
 
-print("test pulling and sync v2")
+print("TESTING BRANCH CHANGES ON LOCAL 9/7")
 
 print("testing sync - client 2")
 print("testing sync issues - client 1")
-<<<<<<< HEAD
 
-print("test local client")
-=======
->>>>>>> df55a84da3d00cf687b140f2d0d2d31bf27620cd
-
-print("test remote client")
->>>>>>> cd81b7851eab362c7e78413a1cd50abf53239db0
+print("testing branch changes on local")
